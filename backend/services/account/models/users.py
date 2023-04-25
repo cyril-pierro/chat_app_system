5a8e4b6fa573813f261e253a9057ce5d8cafe5c0
Cyril Pierro <fiopapa32@gmail.com>
2023-04-25 20:07:35 +0000

    add user and account models

---
+++ backend/services/account/models/users.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+import sqlalchemy as sq
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from core import setup
+
+
+class Users(setup.Base):
+    __tablename__ = "users"
+    id = sq.Column(sq.Integer, primary_key=True)
+    username = sq.Column(
+        sq.String,
+        unique=True,
+        nullable=False,
+    )
+    email = sq.Column(sq.String, nullable=False)
+    hash_password = sq.Column(sq.String, nullable=False)
+    joined_at = sq.Column(sq.DateTime, nullable=False, default=datetime.now())
+
+    def __str__(self) -> str:
+        return f"{self.id}-{self.username}"
+
+    @staticmethod
+    def generate_hash_password(password: str) -> str:
+        encrypted_password = generate_password_hash(password)
+        return encrypted_password
+
+    @staticmethod
+    def verify_password(hash_password: str, password: str) -> bool:
+        return check_password_hash(hash_password, password)
