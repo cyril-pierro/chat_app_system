5277b329f4095fbc1ee2fd6c82bc85b929fe3f2f
Cyril Pierro <fiopapa32@gmail.com>
2023-04-25 20:15:29 +0000

    add unit and functional tests

---
+++ backend/services/account/tests/test_data.py
@@ -0,0 +1,12 @@
+"""Test Data 
+This module defines all custom data that are list, dictionary or set or variabes
+used for testing
+"""
+
+test_user_data = {
+    "username": "test_user",
+    "password": "test_password",
+    "email": "test_email@test.com",
+}
+
+test_login_data = {"username": "test_user", "password": "test_password"}
