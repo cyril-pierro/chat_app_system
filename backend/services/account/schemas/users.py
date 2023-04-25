f785b078994ad273db8888bc36599093cabd2402
Cyril Pierro <fiopapa32@gmail.com>
2023-04-25 20:12:18 +0000

    add schemas

---
+++ backend/services/account/schemas/users.py
@@ -0,0 +1,74 @@
+"""Users Schemas
+
+This module contains all schemas relating to users
+"""
+
+from pydantic import BaseModel, EmailStr
+
+
+class RegisterUser(BaseModel):
+    """Register User Schema
+    Attributes:
+        email (str): email for the payload
+        username (str): username for the payload
+        profile_pic (str): profile_pic for the payload
+    """
+    username: str
+    email: EmailStr
+    password: str
+
+    class Config:
+        orm_mode = True
+
+
+class User(BaseModel):
+    """User  Schema
+    Attributes:
+        email (str): email for the payload
+        username (str): username for the payload
+    """
+    username: str
+    email: EmailStr
+
+    class Config:
+        orm_mode = True
+
+
+class ChangeEmail(BaseModel):
+    """Change Email Schema
+    Attributes:
+        email (str): email for the payload
+    """
+    email: EmailStr
+
+
+class ChangeUsername(BaseModel):
+    """Change Username Schema
+    Attributes:     
+        username (str): username for the payload
+    """
+    username: str
+
+
+class ChangePassword(BaseModel):
+    """Change Password Schema
+    Attributes:
+        password (str): password for the payload
+
+    """
+    password: str
+
+
+class ChangeOut(BaseModel):
+    """Change Out Schema
+
+    This schema is used for all
+    return operations that involve
+    change
+
+    Note - Output Schema for change operations
+
+    Attributes:
+        message (str): message for the payload
+    """
+    message: str
