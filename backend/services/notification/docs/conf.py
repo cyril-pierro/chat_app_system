ed1521a080b7210eb8d055c00249dc344bd0e8f5
Cyril Pierro <fiopapa32@gmail.com>
2023-04-27 14:51:16 +0000

    format codebase

--- backend/services/notification/docs/conf.py
+++ backend/services/notification/docs/conf.py
@@ -8,34 +8,34 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+
+sys.path.insert(0, os.path.abspath(".."))
 from dotenv import load_dotenv
 
 load_dotenv()
 
-project = 'Chat Application (Notification Version)'
-copyright = '2023, Cyril Pierro'
-author = 'Cyril Pierro'
-release = '0.1.0'
+project = "Chat Application (Notification Version)"
+copyright = "2023, Cyril Pierro"
+author = "Cyril Pierro"
+release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-'sphinx.ext.autodoc',
-'sphinx.ext.viewcode',
-'sphinx.ext.napoleon',
-'sphinx.ext.autosummary'
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autosummary",
 ]
 
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
