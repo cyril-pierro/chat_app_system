7361da169bbda9a9daf864e757d66ed95b1cf9db
Cyril Pierro <fiopapa32@gmail.com>
2023-05-04 15:28:37 +0000

    change port

--- backend/services/notification/scripts/startup.sh
+++ backend/services/notification/scripts/startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-celery -A main worker --loglevel=INFO & celery -A main flower --loglevel=INFO --address=0.0.0.0 --port=8083
+celery -A main worker --loglevel=INFO & celery -A main flower --loglevel=INFO --address=0.0.0.0 --port=8082
