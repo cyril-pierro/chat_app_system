a1234c37313393570724e925e3110de3bb96920b
Cyril Pierro <fiopapa32@gmail.com>
2023-05-05 16:53:45 +0000

    add more test to websocket

--- backend/services/chat/websocket/tests/test_websocket.py
+++ backend/services/chat/websocket/tests/test_websocket.py
@@ -3,7 +3,7 @@
 This module demonstrate the operations to test
 websocket connection
 """
-from unittest.mock import patch
+from unittest.mock import Mock, patch
 
 import pytest
 
@@ -24,7 +24,7 @@ async def test_websocket_connect(websocket_instance):
 
 
 @pytest.mark.asyncio
-async def test_websocket_send_and_receive(websocket_instance):
+async def test_websocket_connection_message(websocket_instance):
     websocket = await websocket_instance
 
     with patch(
@@ -42,7 +42,34 @@ async def test_websocket_send_and_receive(websocket_instance):
 
 
 @pytest.mark.asyncio
+async def test_websocket_send_and_recieve(websocket_instance):
+    websocket = await websocket_instance
+
+    with patch("websocket.controller.consumer.requests.get") as mock_request:
+        mock_response = Mock()
+        mock_request.return_value = mock_response
+        mock_response.json.return_value = {"message": "test_user"}
+        mock_response.status_code = 200
+        with patch(f"{CUSTOMER_MODULE_LOC}.get_room_name", return_value="ghana"):
+            _, _ = await websocket.connect()
+            # skip connection message
+            _ = await websocket.receive_json_from()
+            await websocket.send_json_to(
+                {"message": "hello there", "user": "test_user"}
+            )
+            response = await websocket.receive_json_from()
+            assert response == {
+                "type": "chat_message",
+                "message": "hello there",
+                "user": "test_user",
+            }
+
+            await websocket.disconnect()
+
+
+@pytest.mark.asyncio
 async def test_websocket_disconnect(websocket_instance):
     websocket = await websocket_instance
     await websocket.disconnect()
     assert await websocket.receive_nothing()
+
