152c136ae833f8a7d97572f03dcc167759323b1a
Cyril Pierro <fiopapa32@gmail.com>
2023-05-09 09:40:11 +0000

    add support route test

--- backend/services/chat/websocket/tests/conftest.py
+++ backend/services/chat/websocket/tests/conftest.py
@@ -1,13 +1,11 @@
-import pytest
-
 """Configuration for test
 
 This module shows the configuration
 done before you run a test
 
 """
+import pytest
 from channels.testing import WebsocketCommunicator
-
 from websocket.controller import consumer
 
 
@@ -19,3 +17,13 @@ async def websocket_instance():
         consumer.ChatConsumer.as_asgi(), f"/ws/chat/ghana/?token={test_token}"
     )
     return communicator
+
+
+@pytest.fixture
+@pytest.mark.asyncio
+async def websocket_support_instance():
+    test_token = "ghana"
+    support_comm = WebsocketCommunicator(
+        consumer.ChatConsumer.as_asgi(), f"/ws/chat/support/?token={test_token}"
+    )
+    return support_comm
--- backend/services/chat/websocket/tests/test_websocket.py
+++ backend/services/chat/websocket/tests/test_websocket.py
@@ -68,6 +68,36 @@ async def test_websocket_send_and_recieve(websocket_instance):
 
 
 @pytest.mark.asyncio
+async def test_websocket_support_route(websocket_support_instance):
+    websocket = await websocket_support_instance
+
+    with patch("websocket.controller.consumer.requests.get") as mock_request:
+        mock_response = Mock()
+        mock_request.return_value = mock_response
+        mock_response.json.return_value = {"message": "test_user"}
+        mock_response.status_code = 200
+        with patch(f"{CUSTOMER_MODULE_LOC}.get_room_name", return_value="support"):
+            _, _ = await websocket.connect()
+            # skip connection message
+            _ = await websocket.receive_json_from()
+
+            with patch("websocket.controller.consumer.requests.post") as m_post:
+                mock_response = Mock()
+                m_post.return_value = mock_response
+                mock_response.status_code = 200
+                mock_response.json.return_value = {"answer": "Hey"}
+                await websocket.send_json_to({"message": "hello", "user": "test_user"})
+                response = await websocket.receive_json_from()
+                assert response == {
+                    "type": "chat_bot_message",
+                    "message": "Hey",
+                    "user": "Dave",
+                }
+
+                await websocket.disconnect()
+
+
+@pytest.mark.asyncio
 async def test_websocket_disconnect(websocket_instance):
     websocket = await websocket_instance
     await websocket.disconnect()
