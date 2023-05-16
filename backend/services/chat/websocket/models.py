7f932303ae1997e6cc9c237b1a18e08f81dead47
Cyril Pierro <fiopapa32@gmail.com>
2023-05-16 11:26:41 +0000

    fix black issue

--- backend/services/chat/websocket/models.py
+++ backend/services/chat/websocket/models.py
@@ -1,3 +1,22 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    from_author = models.CharField(max_length=200)
+    to_author = models.CharField(max_length=200)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.message
+
+    @staticmethod
+    def last_20_messages_from_author_to_author(from_author, to_author):
+        messages = Message.objects.raw(
+            """SELECT * FROM websocket_message
+                                        WHERE from_author= %s and to_author=%s or from_author=%s and to_author=%s order by created_at asc limit 30""",
+            [from_author, to_author, to_author, from_author],
+        )
+        if len(messages) < 1:
+            messages = []
+        return messages
