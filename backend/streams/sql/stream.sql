979c91229a34868f5892e390a184306453da4798
Cyril Pierro <fiopapa32@gmail.com>
2023-05-17 16:15:40 +0000

    fix join query

--- backend/streams/sql/stream.sql
+++ backend/streams/sql/stream.sql
@@ -5,12 +5,16 @@ create stream created_users_stream with (kafka_topic='created_users',value_forma
 
 create stream account_stream with (kafka_topic='users_topic.public.account',value_format='avro');
 
-create stream create_account_stream with (kafka_topic='created_account', value_format='json') as select after->user_id as user_id, after->profile_pic as profile_pic, after->is_active as is_active from account_stream where op = 'u';
+create stream create_account_stream with (kafka_topic='created_account', value_format='json') as select after->user_id as user_id, after->profile_pic as profile_pic, after->is_active as is_active from account_stream where op in ('c','u');
 
 
 create stream user_to_account as select * from created_users_stream users inner join create_account_stream account within 7 days grace period 30 minutes on users.user_id = account.user_id;
 
-create stream elastic_users_stream with (kafka_topic='elasticsearch_users', value_format='json') as
+create stream elastic_users_stream_with_key with (kafka_topic='elasticsearch_users_with_key', value_format='json') as
 select users_user_id as user_id, users_username as username, users_star as star, account_profile_pic as profile_pic, account_is_active as is_active from user_to_account where users_is_email_verified = true;
 
-select * from ELASTIC_USERS_STREAM emit changes;
+create stream elasticsearch_users_stream_keyless(username varchar, star boolean, profile_pic varchar, is_active boolean) with (kafka_topic='elasticsearch_users_with_key', value_format='json');
+
+create stream elasticsearch_users_stream with (kafka_topic='elasticsearch_users', value_format='json') as select username, star, profile_pic, is_active from elasticsearch_users_stream_keyless;
+
+select * from elasticsearch_users_stream emit changes;
