6d5bb59fa1a62301fb05117e9517ed4fb04e00a5
Cyril Pierro <fiopapa32@gmail.com>
2023-05-17 07:45:31 +0000

    create a join stream for users

--- backend/streams/sql/stream.sql
+++ backend/streams/sql/stream.sql
@@ -1,6 +1,16 @@
 SET 'auto.offset.reset'='earliest';
 create stream users_stream with (kafka_topic='users_topic.public.users',value_format='avro');
 create stream created_users_stream with (kafka_topic='created_users',value_format='json') as select after->id as user_id, after->username as username,after->email as email, after->hash_password as password, after->is_a_star as star, after->is_admin as is_admin, after->is_email_verified as is_email_verified, after->joined_at as joined_at from users_stream where op in ('c', 'u');
-create stream elastic_users_stream with (kafka_topic='elasticsearch_users', value_format='json') as select user_id, username, star from created_users_stream where is_email_verified = true;
+
+
+create stream account_stream with (kafka_topic='users_topic.public.account',value_format='avro');
+
+create stream create_account_stream with (kafka_topic='created_account', value_format='json') as select after->user_id as user_id, after->profile_pic as profile_pic, after->is_active as is_active from account_stream where op = 'u';
+
+
+create stream user_to_account as select * from created_users_stream users inner join create_account_stream account within 7 days grace period 30 minutes on users.user_id = account.user_id;
+
+create stream elastic_users_stream with (kafka_topic='elasticsearch_users', value_format='json') as
+select users_user_id as user_id, users_username as username, users_star as star, account_profile_pic as profile_pic, account_is_active as is_active from user_to_account where users_is_email_verified = true;
 
 select * from ELASTIC_USERS_STREAM emit changes;
