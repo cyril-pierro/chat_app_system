fa28856ec83d51383ea49adff1547c1cbbeed49a
Cyril Pierro <fiopapa32@gmail.com>
2023-05-19 17:07:21 +0000

    fix pre commit

--- backend/services/chat/search/tests/test_views.py
+++ backend/services/chat/search/tests/test_views.py
@@ -4,30 +4,30 @@ from unittest.mock import patch
 
 import pytest
 from django.urls import reverse
-from rest_framework.test import APIClient
-
-client = APIClient()
+from rest_framework.test import APITestCase
 
 
-@pytest.mark.search
-def test_search_view() -> None:
-    """Test Search View"""
-    with patch("search.views.es") as mock_elastic:
-        mock_elastic.search.return_value = {
-            "hits": {"hits": [{"_source": {"USERNAME": "fiopapa"}}]}
-        }
-        response = client.get(reverse("search", args=("fiopapa",)))
-        assert response.json() == [{"USERNAME": "fiopapa"}]
-        assert response.status_code == 200
+class TestViews(APITestCase):
+    """Test View Class"""
 
+    @pytest.mark.search
+    def test_search_view(self) -> None:
+        """Test Search View"""
+        with patch("search.views.es") as mock_elastic:
+            mock_elastic.search.return_value = {
+                "hits": {"hits": [{"_source": {"USERNAME": "fiopapa"}}]}
+            }
+            response = self.client.get(reverse("search", args=("fiopapa",)))
+            assert response.json() == [{"USERNAME": "fiopapa"}]
+            assert response.status_code == 200
 
-@pytest.mark.search
-def test_all_users_view() -> None:
-    """Test All users View"""
-    with patch("search.views.es") as mock_elastic:
-        mock_elastic.search.return_value = {
-            "hits": {"hits": [{"_source": {"USERNAME": "fiopapa"}}]}
-        }
-        response = client.get(reverse("users"))
-        assert response.json() == [{"USERNAME": "fiopapa"}]
-        assert response.status_code == 200
+    @pytest.mark.search
+    def test_all_users_view(self) -> None:
+        """Test All users View"""
+        with patch("search.views.es") as mock_elastic:
+            mock_elastic.search.return_value = {
+                "hits": {"hits": [{"_source": {"USERNAME": "fiopapa"}}]}
+            }
+            response = self.client.get(reverse("users"))
+            assert response.json() == [{"USERNAME": "fiopapa"}]
+            assert response.status_code == 200
