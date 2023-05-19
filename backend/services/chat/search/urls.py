7649d38e349ad1530f5084d0d0d3387a66ae03a9
Cyril Pierro <fiopapa32@gmail.com>
2023-05-19 14:23:29 +0000

    add name variable

--- backend/services/chat/search/urls.py
+++ backend/services/chat/search/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from search import views
 
 urlpatterns = [
-    path("user/<str:username>", views.SearchUsers.as_view()),
+    path("user/<str:username>", views.SearchUsers.as_view(), name="search"),
     path("users/", views.AllUsers.as_view(), name="users"),
 ]
