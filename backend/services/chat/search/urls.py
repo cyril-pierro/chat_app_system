05669041ad47e3bbb05c281d7dec7615cb848905
Cyril Pierro <fiopapa32@gmail.com>
2023-05-19 15:24:47 +0000

    add app name

--- backend/services/chat/search/urls.py
+++ backend/services/chat/search/urls.py
@@ -4,6 +4,6 @@ from search import views
 app_name = "search"
 
 urlpatterns = [
-    path("user/<str:username>", views.SearchUsers.as_view(), name="user"),
+    path("user/<str:username>", views.SearchUsers.as_view(), name="search"),
     path("users/", views.AllUsers.as_view(), name="users"),
 ]
