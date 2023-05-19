9bcb0b04d5cc022ef532c83cb5887e3332968708
Cyril Pierro <fiopapa32@gmail.com>
2023-05-19 15:11:33 +0000

    format codebase

--- backend/services/chat/websocket/models.py
+++ backend/services/chat/websocket/models.py
@@ -15,12 +15,11 @@ class Message(models.Model):
 
     @staticmethod
     def last_20_messages_from_author_to_author(
-        from_author: str, to_author: str  # type :ignore
+        from_author: str, to_author: str
     ) -> list[Any]:
         messages: list[Any] = Message.objects.raw(
             """SELECT * FROM websocket_message
-                        WHERE from_author= %s and to_author=%s or from_author=%s and to_author=%s  # noqa
-                        ORDER BY created_at ASC LIMIT 30""",
+                                        WHERE from_author= %s and to_author=%s or from_author=%s and to_author=%s order by created_at asc limit 30""",  # noqa
             [from_author, to_author, to_author, from_author],
         )
         if len(messages) < 1:
