c859ac27003dae120e0c28e3cb90ef6de12763e2
Cyril Pierro <fiopapa32@gmail.com>
2023-06-16 09:16:08 +0000

    update script

--- backend/services/notification/scripts/startup.sh
+++ backend/services/notification/scripts/startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-celery -A main worker --loglevel=INFO & celery -A main flower --loglevel=INFO --address=0.0.0.0 --port=8082
+celery -A main worker -b $CELERY_BROKER_URL --loglevel=INFO & sleep 4s; celery -A main flower -b $CELERY_BROKER_URL --loglevel=INFO --address=0.0.0.0 --port=8082
